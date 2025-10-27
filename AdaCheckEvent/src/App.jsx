80681e7b77d8835e52c84d5236b63f5870a450a5
Yannick Souza <darkvalot@gmail.com>
2025-10-27 15:30:31 +0100

    working on offset argument

--- AdaCheckEvent/src/App.jsx
+++ AdaCheckEvent/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, use } from 'react'
 import './App.css'
 
 function App() {
   const [data, setData] = useState(null)
   const [error, setError] = useState(null)
-
+  const [offset, setOffset] = useState(0)
   useEffect(() => {
     const controller = new AbortController()
 
-    const fetchData = async () => {
+    const fetchData = async (offset) => {
       try {
-        const response = await fetch('https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=1&offset=2', {
+        const response = await fetch(`https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=2&offset=${offset}`, {
           signal: controller.signal
         })
         const dataFetch = await response.json()
@@ -25,7 +25,7 @@ function App() {
       }
     }
 
-    fetchData()
+    fetchData(offset)
 
     return () => controller.abort()
   }, [])
@@ -37,7 +37,13 @@ function App() {
     <div className="App">
       <h1>Événements à Paris</h1>
       <p> {data.map((elem) => {
-        return (<span key={elem.id}>{elem.title} <img src={elem.cover_url} alt={elem.title} />{elem.description}</span>)
+        return (
+          <>
+        <span key={elem.id}>{elem.title} <img src={elem.cover_url} alt={elem.title} />{elem.description}</span>
+        <button onClick={() => fetchData(2)} className='bg-blue-500 rounded'>test</button>
+          </>
+
+        )
       })} </p>
       </div>
   )
